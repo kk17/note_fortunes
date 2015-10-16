ac942055256e03e7ed9502a422ae729222de4261
Zhike Chan <zk.chan007@gmail.com>
2015-10-16 21:46:55 +0800

    improved install script

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
 
+python note2data.py
+strfile data/*
+base_dir=`pwd`
+# echo $base_dir
+bin_path=`which fortune`
+link_path=`readlink $bin_path`
+# echo $bin_path
+cd `dirname $bin_path`
+cd `dirname $link_path`
+real_path=`pwd`/../share/games/fortunes
+# echo $real_path
+cd $base_dir
 # python ./note2data.py
-sudo cp data/* /usr/share/games/fortunes/
+cp data/* $real_path
 fortune 100% refactor_your_wetware
-
-
--- note2data.py
+++ note2data.py
@@ -3,54 +3,52 @@
 
 import os
 import os.path as path
-import sh
 
 NOTES_DIR = "notes"
 DATA_DIR = "data"
 
 
 def note2data(note_filename):
-	basename = path.basename(note_filename)
-	base = path.splitext(basename)[0]
-	# print base
-	infilepath = path.join(NOTES_DIR,note_filename)
-	outfilepath = path.join(DATA_DIR,base)
-	# print infilepath
-	with open (infilepath,"r") as fi, open (outfilepath, "w") as fo:
-		bookname = None
-		lines_blocks = None
+    basename = path.basename(note_filename)
+    base = path.splitext(basename)[0]
+    # print base
+    infilepath = path.join(NOTES_DIR, note_filename)
+    outfilepath = path.join(DATA_DIR, base)
+    # print infilepath
+    with open(infilepath,"r") as fi, open (outfilepath, "w") as fo:
+        bookname = None
+        lines_blocks = None
 
-		def write_block(append_sign=True):
-			if lines_blocks:
-				if bookname:
-					lines_blocks.append(bookname)
-				if append_sign:
-					lines_blocks.append("%\n")
-				fo.writelines(lines_blocks)
+        def write_block(append_sign=True):
+            if lines_blocks:
+                if bookname:
+                    lines_blocks.append(bookname)
+                if append_sign:
+                    lines_blocks.append("%\n")
+                fo.writelines(lines_blocks)
 
-		for line in fi:
-			if not line.strip():
-				continue
-			if line.startswith("#") and line[1] != "#":
-				bookname = line.replace("#","",1)
-				# print bookname
-			elif line.startswith("##") and line[2] != "#":
-				write_block()
-				line = line.replace("##","",1)
-				# print line
-				lines_blocks = []
-				lines_blocks.append(line)
-			else:
-				lines_blocks.append(line)
-			# print line
-		write_block(False)
-		sh.strfile(outfilepath)
+        for line in fi:
+            if not line.strip():
+                continue
+            if line.startswith("#") and line[1] != "#":
+                bookname = line.replace("#","",1)
+                # print bookname
+            elif line.startswith("##") and line[2] != "#":
+                write_block()
+                line = line.replace("##","",1)
+                # print line
+                lines_blocks = []
+                lines_blocks.append(line)
+            else:
+                lines_blocks.append(line)
+            # print line
+        write_block(False)
+
 
 def allnote2data():
-	for filename in os.listdir(NOTES_DIR):
-		note2data(filename)
+    for filename in os.listdir(NOTES_DIR):
+        note2data(filename)
 
 if __name__ == '__main__':
-	# note2data("refactor_your_wetwart.md")
-	allnote2data()
-
+    # note2data("refactor_your_wetwart.md")
+    allnote2data()
